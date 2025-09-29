a6c4ce7154a38229a3286da706bedd1e5659d658
Lume <selvanasameh7@gmail.com>
2025-09-29 23:13:52 +0300

    Update Forecast.tsx

--- src/app/dashboard/weather/Forecast.tsx
+++ src/app/dashboard/weather/Forecast.tsx
@@ -7,86 +7,85 @@ import Image from 'next/image'
 import React from 'react'
 
 type ForCastProps = {
-   data:Forecast []
-    
+  data: ForecastType[]
 }
-const Forecast = ({data}:ForCastProps) => {
+
+const Forecast = ({ data }: ForCastProps) => {
   return (
     <section className='mt-20 mb-16 w-full'>
-        <Button className='font-bold capitalize text-2xl py-6 mb-5'>Hourly Weather Forecast</Button>
+      <Button className='font-bold capitalize text-2xl py-6 mb-5'>
+        Hourly Weather Forecast
+      </Button>
 
-        {data?.map((item, index)=>
-        
-        <>
-        <div key={index} className='flex items-center justify-between py-5'>
-             <TolTip content='Time'>
+      {data?.map((item, index) => (
+        <React.Fragment key={index}>
+          <div className='flex items-center justify-between py-5'>
+            <TolTip content='Time'>
+              <span>{item.time}</span>
+            </TolTip>
 
-            <span>{item.time}</span>
-             </TolTip>
-            
             <TolTip content='Temperature'>
-
-            <span>{item.temp}&deg;C</span>
+              <span>{item.temp}&deg;C</span>
             </TolTip>
-            
+
             <TolTip content='Humidity'>
-                
-                
-            <span className='flex items-center gap-1'>
-                <Image src={'/humidity.svg'} alt='humidity' width={14} height={14}/>
+              <span className='flex items-center gap-1'>
+                <Image
+                  src={'/humidity.svg'}
+                  alt='humidity'
+                  width={14}
+                  height={14}
+                />
                 <span>
-
-                {item.humidity}
-                <span>%</span>
+                  {item.humidity}
+                  <span>%</span>
                 </span>
-            </span>
+              </span>
             </TolTip>
+
             <TolTip content='Rain status'>
-
-            <span className='flex items-center gap-1'>
-              <CloudFog className='text-primary' size={20}/>
-                {item.rain_probability}<sub>km/h</sub>
-            </span>
+              <span className='flex items-center gap-1'>
+                <CloudFog className='text-primary' size={20} />
+                {item.rain_probability}
+                <sub>km/h</sub>
+              </span>
             </TolTip>
+
             <TolTip content='Direction'>
-
-            <Button className='font-semibold'>
-            {item.wind_direction}
-            </Button>
-              
+              <Button className='font-semibold'>{item.wind_direction}</Button>
             </TolTip>
+
             <TolTip content='Rain'>
-
-            <span className='flex items-center gap-1 mr-10'>
-                <Image src={'/rain-drobs.svg'} alt='humidity' width={14} height={14}/>
+              <span className='flex items-center gap-1 mr-10'>
+                <Image
+                  src={'/rain-drobs.svg'}
+                  alt='rain'
+                  width={14}
+                  height={14}
+                />
                 NO
-            </span>
+              </span>
             </TolTip>
-        </div>
-        <Separator />
-        </>
-        )}
+          </div>
+          <Separator />
+        </React.Fragment>
+      ))}
     </section>
   )
 }
 
 export default Forecast
 
-
 interface TolTipProps {
-    children:React.ReactNode,
-    content:string
+  children: React.ReactNode
+  content: string
 }
 
-const TolTip = ({children, content}:TolTipProps) => {
+const TolTip = ({ children, content }: TolTipProps) => {
   return (
     <Tooltip>
-      <TooltipTrigger asChild>
-        {children}
-      </TooltipTrigger>
-      <TooltipContent>
-        {content}
-      </TooltipContent>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipContent>{content}</TooltipContent>
     </Tooltip>
   )
 }
