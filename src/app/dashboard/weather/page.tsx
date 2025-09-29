84d7457c5b2e58741b8fff3baa4280c8548d3402
Lume <selvanasameh7@gmail.com>
2025-09-30 00:06:44 +0300

    Update page.tsx

--- src/app/dashboard/weather/page.tsx
+++ src/app/dashboard/weather/page.tsx
@@ -1,18 +1,17 @@
-'use client';
+'use client'
 
 import Footer from "@/components/custom/large/Footer";
 import Forecast from "./Forecast";
 import Map from "./Map";
 import WeatherCard from "./WeatherCard";
 import { Card } from "@/components/ui/card";
-
 import Logo from "@/components/custom/atoms/Logo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { LocateFixed, MapPin, Search } from "lucide-react";
 import { fetchWeather } from "@/services/dashboard";
 import { useState } from "react";
-import { WeatherData } from "@/lib/types";
+import { WeatherData } from '@/lib/types';
 import Loading from "./loading";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
@@ -34,13 +33,12 @@ const Dashboard = () => {
     },
   });
 
-  const hanldeSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget as HTMLFormElement;
     const cityInput = form.elements.namedItem("city") as HTMLInputElement;
     const newCity = cityInput.value.trim();
     if (!newCity) return;
-
     await mutate.mutateAsync(newCity);
     setCity(newCity);
   };
@@ -48,82 +46,72 @@ const Dashboard = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <>
-      <section>
-        <header className="flex justify-between items-center px-4 w-11/12 max-w-7xl mx-auto">
-          <div className="flex-1">
-            <Logo />
-          </div>
-
-          <div className="flex-1 flex items-center justify-between">
-            <div className="flex items-center gap-1">
-              <MapPin />
-              <span className="text-xl">
-                {data?.current_weather?.city ||
-                  data?.location?.name ||
-                  "Unknown City"}
-                ,{" "}
-                {data?.current_weather?.country ||
-                  data?.location?.country ||
-                  "??"}
-              </span>
-            </div>
-
-            <form
-              onSubmit={hanldeSearch}
-              className="flex items-center relative w-full max-w-sm"
-            >
-              <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
-              <Input
-                name="city"
-                type="search"
-                placeholder="Search City"
-                className="w-full pl-10 pr-4 rounded-tr-none rounded-br-none"
-              />
-              <Button
-                type="submit"
-                size="icon"
-                className="py-3 rounded-tl-none rounded-bl-none"
-              >
-                <LocateFixed />
-              </Button>
-            </form>
-          </div>
-        </header>
-
-        <div className="max-w-6xl w-11/12 mx-auto px-8">
-          {data != null ? (
-            <>
-              <div className="flex items-center gap-16 justify-between mt-8">
-                <WeatherCard
-                  current_temp={data.current_weather.current_temp}
-                  feels_like={data.current_weather.feels_like}
-                  humidity={data.current_weather.humidity}
-                  pressure={data.current_weather.pressure}
-                  rain_prediction={data.current_weather.rain_prediction}
-                  temp_max={data.current_weather.max_temp}
-                  temp_min={data.current_weather.min_temp}
-                  wind_direction={data.current_weather.wind_direction}
-                  wind_speed={data.current_weather.wind_speed}
-                />
-                <Map
-                  map={data?.map_html}
-                  lat={data.latitude}
-                  long={data.longitude}
-                />
-              </div>
-              <Forecast data={data.forecast} />
-            </>
-          ) : (
-            <div className="c-flex h-[70vh]">
-              <Card className="px-10 py-5">
-                City not found. Please try again.
-              </Card>
-            </div>
-          )}
+    <section>
+      <header className="flex justify-between items-center px-4 w-11/12 max-w-7xl mx-auto">
+        <div className="flex-1">
+          <Logo />
         </div>
-      </section>
-    </>
+        <div className="flex-1 flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <MapPin />
+            <span className="text-xl">
+              {data?.current_weather.city}, {data?.current_weather.country}
+            </span>
+          </div>
+          <form
+            onSubmit={handleSearch}
+            className="flex items-center relative w-full max-w-sm"
+          >
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+            <Input
+              name="city"
+              type="search"
+              placeholder="Search City"
+              className="w-full pl-10 pr-4 rounded-tr-none rounded-br-none"
+            />
+            <Button
+              type="submit"
+              size={"icon"}
+              className="py-3 rounded-tl-none rounded-bl-none"
+            >
+              <LocateFixed />
+            </Button>
+          </form>
+        </div>
+      </header>
+
+      <div className="max-w-6xl w-11/12 mx-auto px-8">
+        {data != null ? (
+          <>
+            <div className="flex items-center gap-16 justify-between mt-8">
+              <WeatherCard
+                current_temp={data.current_weather.current_temp}
+                feels_like={data.current_weather.feels_like}
+                humidity={data.current_weather.humidity}
+                pressure={data.current_weather.pressure}
+                rain_prediction={data.current_weather.rain_prediction}
+                temp_max={data.current_weather.max_temp}
+                temp_min={data.current_weather.min_temp}
+                wind_direction={data.current_weather.wind_direction}
+                wind_speed={data.current_weather.wind_speed}
+              />
+              <Map
+                map={data?.map_html}
+                lat={data.latitude}
+                long={data.longitude}
+              />
+            </div>
+            <Forecast data={data.forecast} />
+          </>
+        ) : (
+          <div className="c-flex h-[70vh]">
+            <Card className="px-10 py-5">
+              City not found. Please try again.
+            </Card>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
